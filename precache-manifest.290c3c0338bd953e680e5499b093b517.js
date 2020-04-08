3d6f48e33c0d323c6b73bb165d7d75eed3f55c35
Kelum Sampath Edirisinghe <edirisingheemks@gmail.com>
2020-04-08 13:39:31 +0530

    Updates

---
+++ precache-manifest.290c3c0338bd953e680e5499b093b517.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9487745f7c90ebce61570df18feddd4",
+    "url": "/intecs_webapp_front-end/index.html"
+  },
+  {
+    "revision": "29becf178876898a6ab6",
+    "url": "/intecs_webapp_front-end/static/css/main.8cb2348f.chunk.css"
+  },
+  {
+    "revision": "8255f69cada9d3c61729",
+    "url": "/intecs_webapp_front-end/static/js/2.e6677e77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/intecs_webapp_front-end/static/js/2.e6677e77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29becf178876898a6ab6",
+    "url": "/intecs_webapp_front-end/static/js/main.5e9afae0.chunk.js"
+  },
+  {
+    "revision": "f7691ed928ab0256f50c",
+    "url": "/intecs_webapp_front-end/static/js/runtime-main.e5654daa.js"
+  }
+]);
